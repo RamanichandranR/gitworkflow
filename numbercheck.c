21ec0b0ca1b5aae54244f46ce8bb7ac63cca673c
ramani <ramanirmech@gmail.com>
2019-10-18 22:12:48 +0530

    added 2 files

---
+++ numbercheck.c
@@ -0,0 +1,7 @@
+Read isfive
+If(isfive = 5)
+Write "your number is 5"
+Else if (isfive = 6)
+Write "your number is 6"
+Else
+Write "your number is not 5 or 6"
---
+++ printcolor.c
@@ -0,0 +1,8 @@
+Write "Please enter a number"
+Read colornum
+If (colornum >0 and colornum <= 10)
+Write blue
+else If (colornum >0 and colornum <= 10)
+Write blue
+else
+Write "not a correct color option"
